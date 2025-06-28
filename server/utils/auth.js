8a37f3456020b625465afda1dbaf4228da4319a5
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-28 23:27:28 +0530

    Implemnted function generateTokenAndSetCookie to generate token from jwt and set it in cookies

---
+++ server/utils/auth.js
@@ -0,0 +1,14 @@
+export const generateTokenAndSetCookie = async (user, res) => {
+
+    const token = user.generateToken(user._id);
+    const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // Ensures cookie security in production
+        sameSite: "strict",
+        maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+    }
+
+    //set cookie in response so that this token can be sent with every request and we can autheticate user on basis of this token stored in cookie
+    res.cookie("token", token, options);
+
+}

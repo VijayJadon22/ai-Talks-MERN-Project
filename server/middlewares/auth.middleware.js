d9c7a19c828ecab3586b6e67ca95a13952b6a3b5
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-29 23:08:03 +0530

    Implemnted authMiddleware for checking the token in cookies and attaching user to the request

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+export const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({ success: false, message: "Unauthorized" })
+        }
+        const payload = await jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(payload.userId).select("-password");
+        if (!user) {
+            return res.status(401).json({ success: false, message: "User not found" });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Invalid or expired token" });
+    }
+}

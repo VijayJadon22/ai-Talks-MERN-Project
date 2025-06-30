9a44945c374c1736a2445066320117836ab0f0df
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-30 21:45:49 +0530

    Implemnted getUser function using async thunk which will check for user

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "../../utils/axiosConfig.js";
+import toast from "react-hot-toast";
 
 
 const initialState = {
@@ -12,16 +13,29 @@ const initialState = {
 export const registerUser = createAsyncThunk("auth/register", async (userData, thunkAPI) => {
     try {
         const response = await axios.post("/auth/signup", userData);
+        toast.success("Sign up successfull!");
         return response.data.user;
     } catch (error) {
-        return thunkAPI.rejectWithValue(error.response?.data?.message);
+        toast.error(error.response?.message || "Internal server error");
+        return thunkAPI.rejectWithValue(error.message);
     }
 
-})
+});
+
+console.log(initialState);
 
 export const loginUser = createAsyncThunk("auth/login", async (userData, thunkAPI) => {
     try {
         const response = await axios.post("/auth/login", userData);
+        return response.data.user;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response?.data?.message);
+    }
+})
+
+export const getUser = createAsyncThunk("auth/getUser", async (thunkAPI) => {
+    try {
+        const response = await axios.get("auth/");
         return response.data.user;
     } catch (error) {
         return thunkAPI.rejectWithValue(error.response?.data?.message);
@@ -34,16 +48,16 @@ const authSlice = createSlice({
 
     extraReducers: (builder) => {
         builder
-            .addCase(registerUser.pending, loginUser.pending, (state) => {
+            .addCase(registerUser.pending, loginUser.pending, getUser.pending, (state) => {
                 state.isLoading = true;
                 state.error = null;
             })
-            .addCase(registerUser.fulfilled, loginUser.fulfilled, (state, action) => {
+            .addCase(registerUser.fulfilled, loginUser.fulfilled, getUser.fulfilled, (state, action) => {
                 state.user = action.payload;
                 state.isAuthenticated = true;
                 state.isLoading = false;
             })
-            .addCase(registerUser.rejected, (state, action) => {
+            .addCase(registerUser.rejected, loginUser.rejected, getUser.rejected, (state, action) => {
                 state.isLoading = false;
                 state.error = action.payload
             })

ab553f7bab0bf176f6dc358d3d8836137215374f
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-01 14:18:37 +0530

    Increased the days of token expiration to 7 days

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,11 +1,12 @@
 export const generateTokenAndSetCookie = (user, res) => {
+    
 
     const token = user.generateToken(user._id);
     const options = {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // Ensures cookie security in production
         sameSite: "strict",
-        maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     }
 
     //set cookie in response so that this token can be sent with every request and we can autheticate user on basis of this token stored in cookie
